2bcf90041dfe8bd87c913aec9c1184b36618d524
André Grillo <andregrillo@gmail.com>
2024-03-18 16:35:07 +0000

    fixed iOS build crash on MABS 10

--- hooks/before_build/podInstall.js
+++ hooks/before_build/podInstall.js
@@ -1,4 +1,20 @@
 const path = require('path');
+const { execSync } = require('child_process');
+
+module.exports = function (ctx) {
+    const pathiOS = path.join(ctx.opts.projectRoot, "platforms", "ios");
+    
+    try {
+        const result = execSync('pod install', { cwd: pathiOS, stdio: 'inherit' });
+        console.log("✅ Pod install process finished.");
+    } catch (error) {
+        console.error("🚨 ERROR during pod install: ", error);
+        // Consider whether you should throw the error to stop the build process
+        throw error;
+    }
+};
+
+/*const path = require('path');
 var child_process = require('child_process');
 
 module.exports = function (ctx) {
@@ -9,4 +25,4 @@ module.exports = function (ctx) {
     if(child.error) {
         console.log("ERROR: ",child.error);
     }
-}
+}*/
